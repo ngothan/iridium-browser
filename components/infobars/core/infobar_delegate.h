9d946a0374252debc60e8a73fba2fe60d57afbaf
Jan Engelhardt <jengelh@inai.de>
2021-03-07 22:42:50 +0100

    net: add "trk:" scheme and help identify URLs being retrieved
    The Chromium codebase has left us with a number of suspect URLs, and
    we want to know if the browser attempts to contact those sites.
    
    This patch introduces a new scheme, "trk:", which, when attempted to
    being processed, will dump a warning onto the screen as the resource
    is loaded. All URLs we think are suspect are "blacklisted" by
    prepending the new scheme to an existing URL:
    
    	trk:1234:https://clients4.google.com/
    	trk:https://clients4.google.com/ (unnumbered old variant)
    	trk:0.1234:https://... (stderr only, no UI reporting)
    
    Upon seeing a warning, we then know to investigate further, and either
    (a) whitelist the URL, that is, remove the trk: prefix and not show
    the warning, and/or (b) disable the particular feature which caused
    the loading of the URL in the first place, by default.
    
    Implementation:
    
    We hack up the URLFetcher class which sits in the network stack, and
    most of the URL that get loaded pass through here. The trk: prefix is
    stripped and processing continues with the inner URL.

--- components/infobars/core/infobar_delegate.h
+++ components/infobars/core/infobar_delegate.h
@@ -189,6 +189,7 @@ class InfoBarDelegate {
     LOCAL_TEST_POLICIES_APPLIED_INFOBAR = 115,
     BIDDING_AND_AUCTION_CONSENTED_DEBUGGING_DELEGATE = 116,
     PARCEL_TRACKING_INFOBAR_DELEGATE = 117,
+    TRACKING_ALERT_INFOBAR_DELEGATE = 9001,
   };
 
   // Describes navigation events, used to decide whether infobars should be
--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -829,6 +829,7 @@ ChildProcessSecurityPolicyImpl::ChildProcessSecurityPolicyImpl()
   RegisterPseudoScheme(url::kJavaScriptScheme);
   RegisterPseudoScheme(kViewSourceScheme);
   RegisterPseudoScheme(kGoogleChromeScheme);
+  RegisterPseudoScheme(url::kTraceScheme);
 }
 
 ChildProcessSecurityPolicyImpl::~ChildProcessSecurityPolicyImpl() {
